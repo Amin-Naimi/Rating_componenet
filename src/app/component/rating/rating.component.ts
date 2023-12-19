f83e0b153f00585aadfc719c5f1d9f0c4035d632
Mohamed <ahnbi32@gmail.com>
2023-12-19 22:58:03 +0100

    First exemple

--- src/app/component/rating/rating.component.ts
+++ src/app/component/rating/rating.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-rating',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./rating.component.css']
 })
 export class RatingComponent {
+  // si initial Rating = 3 :
+  // i = 1 & i = 2 & i = 3 auront la classe active : i <= initialRating
+  stars=[1,2,3,4,5];
+  initialRating = 0;
+
+  updateRating(rate:number){
+    this.initialRating=rate;
+    console.log(this.initialRating);
+  }
+
 
 }
